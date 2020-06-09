9cf1e9c447ec256a52d8bea3874ed4db3e53afa6
livio <liviocalivers@hotmail.com>
2020-06-09 23:32:00 +0200

    bugfix analysis

--- pdesim_analysis.C
+++ pdesim_analysis.C
@@ -32,12 +32,12 @@ void pdesim_analysis() {
    Double_t prim_Ypos;
    Double_t prim_Zpos;
    Int_t totsteps;
-   vector<Long64_t> *trackid;
-   vector<Long64_t> *parentid;
-   vector<Long64_t> *firstparentid;
-   vector<Int_t> evhits (neve,0);
+   std::vector<Long64_t> * trackid=0;
+   std::vector<Long64_t> * parentid=0;
+   std::vector<Long64_t> * firstparentid=0;
+   std::vector<Int_t> evhits (neve,0);
      
-  tr->SetBranchAddress("eventid",&eventid); //ev number
+  tr->SetBranchAddress("EvId",&eventid); //ev number
   tr->SetBranchAddress("prim_Xpos",&prim_Xpos);
   tr->SetBranchAddress("prim_Ypos",&prim_Ypos);
   tr->SetBranchAddress("prim_Zpos",&prim_Zpos);
@@ -45,13 +45,14 @@ void pdesim_analysis() {
   tr->SetBranchAddress("trackid",&trackid);
   tr->SetBranchAddress("parentid",&parentid);
   tr->SetBranchAddress("firstparentid",&firstparentid);
-  
-   for (Long64_t i=0;i<10; i++) {
+  tr->GetEntry(0);
+   for (Long64_t i=0;i<neve; i++) {
       tr->GetEntry(i);
       Long64_t evlen = trackid->size();
 	  for (Long64_t j=0;j<evlen; j++) {
 		  std::cout << "trackid" << (*trackid)[j] << "parentid" << (*parentid)[j] << "firstparentid" << (*firstparentid)[j] << std::endl;
 	  }
    }
+   
 }
 
