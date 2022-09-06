57fc58d2776ad91b986a6c3965ea6294faf028d3
liviocali <liviocalivers@hotmail.com>
2022-09-05 23:07:43 -0500

    upload files (json,gdml,macro) for single arclight simulations

--- runPDEsim.sh
+++ runPDEsim.sh
@@ -1,1 +1,1 @@
-./build/ArgonCubeOptPh -g resources/gdml/ArCLight_wMirr.gdml -p resources/macros/preinitOptProp_livio.mac -m resources/macros/pdesim.mac -o "$1"
+./build/ArgonCubeOptPh -g resources/gdml/acl_mod123.gdml -p resources/macros/preinit.mac -m resources/macros/pdesim.mac -o pdesim.root
